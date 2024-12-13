8f502fcfcb8719db71a570432a8616be5d48b1f0
Matheus Shiraga Rasera <matheus.s.rasera@gmail.com>
2024-12-12 21:12:12 -0300

    Captcha manual

--- militares.py
+++ militares.py
@@ -3,10 +3,6 @@ import asyncio
 from playwright.async_api import async_playwright
 from dotenv import load_dotenv
 from PIL import Image
-import pytesseract
-
-# Configurar o caminho do executável do Tesseract (ajuste conforme necessário)
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\mathe\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -16,18 +12,12 @@ INITIAL_URL = os.getenv('INITIAL_URL')
 CPF = os.getenv('CPF')
 SENHA = os.getenv('SENHA')
 
-async def solve_captcha(page, captcha_selector):
+async def capture_captcha_image(page, captcha_selector):
     # Captura a imagem do CAPTCHA
     captcha_image_path = "captcha_image.png"
     await page.locator(captcha_selector).screenshot(path=captcha_image_path)
-
-    # Usar o Tesseract OCR para extrair o texto
-    captcha_image = Image.open(captcha_image_path)
-    captcha_text = pytesseract.image_to_string(captcha_image, config="--psm 8")
-
-    # Exibir o texto extraído
-    print(f"CAPTCHA Resolvido: {captcha_text}")
-    return captcha_text.strip()
+    print(f"CAPTCHA image saved as {captcha_image_path}")
+    return captcha_image_path
 
 async def main():
     async with async_playwright() as playwright:
@@ -45,18 +35,25 @@ async def main():
         # Esperar até 15 segundos para o CAPTCHA ser carregado
         await page.wait_for_selector('img[src^="/captcha/"]', timeout=15000)
 
-        # Resolver o CAPTCHA
-        captcha_text = await solve_captcha(page, 'img[src^="/captcha/"]')
+        # Capturar a imagem do CAPTCHA
+        captcha_image_path = await capture_captcha_image(page, 'img[src^="/captcha/"]')
+
+        # Exibir a imagem para o usuário
+        print(f"Please solve the CAPTCHA in the image below: {captcha_image_path}")
+
+        # Solicitar que o usuário insira o texto do CAPTCHA
+        captcha_text = input("Enter the CAPTCHA text: ")
 
         # Inserir o texto do CAPTCHA no campo correspondente
         await page.fill("input[name='captcha[input]']", captcha_text)
 
-        # Clicar no botão de login
-        #await page.click("button[type='submit']")
+        # Clicar no botão de login (descomentado se necessário)
+        await page.click("input[type='submit']")
 
         # Aguarde para verificar o resultado
         await page.wait_for_timeout(10000)
 
+        # Fechar o navegador
         await browser.close()
 
 if __name__ == "__main__":
